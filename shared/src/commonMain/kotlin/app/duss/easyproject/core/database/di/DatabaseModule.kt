e14915fbf74d8065e8bcbffb5cec695b91381ba1
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-11-24 15:43:37 +0100

    Update DatabaseModule.kt

--- shared/src/commonMain/kotlin/app/duss/easyproject/core/database/di/DatabaseModule.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/core/database/di/DatabaseModule.kt
@@ -1,14 +1,12 @@
 package app.duss.easyproject.core.database.di
 
 import app.duss.easyproject.core.database.createDatabase
-import app.duss.easyproject.core.database.dao.PokemonDao
-import app.duss.easyproject.core.database.dao.PokemonInfoDao
+import app.duss.easyproject.core.database.dao.UserDao
 import app.duss.easyproject.core.database.sqlDriverFactory
 import org.koin.dsl.module
 
 val databaseModule = module {
     factory { sqlDriverFactory() }
     single { createDatabase(driver = get()) }
-    single { PokemonDao(pokemonDatabase = get()) }
-    single { PokemonInfoDao(pokemonDatabase = get()) }
+    single { UserDao(appDatabase = get()) }
 }
