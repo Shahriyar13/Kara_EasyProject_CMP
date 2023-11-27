34e157cf7304ec53927588d75670e1a91054e37c
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-11-27 15:44:13 +0100

    Update NetworkConstants.kt

--- shared/src/commonMain/kotlin/app/duss/easyproject/core/network/NetworkConstants.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/core/network/NetworkConstants.kt
@@ -9,10 +9,20 @@ object NetworkConstants {
     object Project {
         private const val route = "$baseApiUrl/project"
         val getAll: String = "$route/v1/getAll"
+        val validateCode: String = "$route/v1/validCode"
         val getById: String = "$route/v1/getById"
+        val getNew: String = "$route/v1/getNew"
         val getAllByCustomerEnquiryId: String = "$route/v1/getAllByCustomerEnquiryId"
         val deleteById: String = "$route/v1/deleteById"
         val deleteAll: String = "$route/v1/deleteAll"
-        val add: String = "$route/v1/add"
+        val create: String = "$route/v1/create"
+        val update: String = "$route/v1/update"
+    }
+    object User {
+        private const val route = "$baseApiUrl/auth"
+        val login: String = "$route/v1/login"
+        val create: String = "$route/v1/create"
+        val update: String = "$route/v1/update"
+        val changeRole: String = "$route/v1/changeRole"
     }
 }
