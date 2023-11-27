62ea1d485f3a06b5159c29f836a1531fa2c790b4
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-11-27 15:44:05 +0100

    Update NetworkModule.kt

--- shared/src/commonMain/kotlin/app/duss/easyproject/core/network/di/NetworkModule.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/core/network/di/NetworkModule.kt
@@ -1,13 +1,15 @@
 package app.duss.easyproject.core.network.di
 
 import app.duss.easyproject.core.network.client.ProjectClient
+import app.duss.easyproject.core.network.client.UserClient
 import app.duss.easyproject.core.network.createHttpClient
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val networkModule: (enableLogging: Boolean) -> Module get() = { enableLogging ->
     module {
-        single { createHttpClient(enableLogging) }
+        single { createHttpClient(get()) }
         single { ProjectClient(httpClient = get()) }
+        single { UserClient(httpClient = get()) }
     }
 }
