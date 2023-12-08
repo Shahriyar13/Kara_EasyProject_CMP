e5687665dd2b13de158c2fd69ff6271f818034a7
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-12-08 14:04:33 +0100

    update images

--- shared/src/commonMain/kotlin/app/duss/easyproject/presentation/ui/dashboard/state/CategoryState.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/presentation/ui/dashboard/state/CategoryState.kt
@@ -19,84 +19,84 @@ data class CategoryState(
     companion object {
         val database = CategoryState(
             title = "Database",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/pokeball.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/dna.png",
             startColor = Red300,
             endColor = Red500,
         )
 
         val ce = CategoryState(
             title = "Customer Enquiries",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/electric.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/enquiry.png",
             startColor = Yellow300,
             endColor = Yellow500,
         )
 
         val project = CategoryState(
             title = "Projects",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/dna.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/location.png",
             startColor = Green300,
             endColor = Green500,
         )
 
         val se = CategoryState(
             title = "Supplier Enquiries",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/location.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val sq = CategoryState(
             title = "Supplier Quotations",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/electric.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val pi = CategoryState(
             title = "Proforma Invoices",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/electric.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val po = CategoryState(
             title = "Purchase Orders",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/electric.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val shipping = CategoryState(
             title = "Shipping Lists",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/shipping.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val invoice = CategoryState(
             title = "Invoices",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/invoice.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val bafa = CategoryState(
             title = "BAFAs",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/bafa.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val payment = CategoryState(
             title = "Payments",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/financial.png",
             startColor = Blue300,
             endColor = Blue500,
         )
 
         val profile = CategoryState(
             title = "Profile And Settings",
-            iconUrl = "https://raw.githubusercontent.com/M0Coding/Pokedex/main/icons/location.png",
+            iconUrl = "https://raw.githubusercontent.com/Shahriyar13/Kara_EasyProject_CMP/featuremvplist/icons/pokeball.png",
             startColor = Blue300,
             endColor = Blue500,
         )
