cdfdd20721d2514a939e1e2ab9520c7ebd1669a7
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-12-10 15:22:33 +0100

    Update ProjectDetailsContent.kt

--- shared/src/commonMain/kotlin/app/duss/easyproject/presentation/ui/project/details/components/ProjectDetailsContent.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/presentation/ui/project/details/components/ProjectDetailsContent.kt
@@ -16,10 +16,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,6 +35,7 @@ import app.duss.easyproject.presentation.components.EditableText
 import app.duss.easyproject.presentation.helper.LocalSafeArea
 import app.duss.easyproject.presentation.ui.project.details.ProjectDetailsComponent
 import app.duss.easyproject.presentation.ui.project.details.store.ProjectDetailsStore
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,6 +45,9 @@ internal fun DetailsContent(
     onOutput: (ProjectDetailsComponent.Output) -> Unit,
     modifier: Modifier,
 ) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     Box(contentAlignment = Alignment.TopCenter) {
 
         if (state.isDeleted) {
@@ -50,6 +59,7 @@ internal fun DetailsContent(
         }
 
         Scaffold(
+            snackbarHost = { SnackbarHost(snackbarHostState) },
             topBar = {
                 TopAppBar(
                     navigationIcon = {
@@ -130,7 +140,13 @@ internal fun DetailsContent(
                 }
 
                 state.error?.let { error ->
-                    Text(text = error, color = MaterialTheme.colorScheme.error)
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            error,
+                            withDismissAction = true,
+                            duration = SnackbarDuration.Indefinite
+                        )
+                    }
                 }
 
                 state.projectForm?.let { item ->
