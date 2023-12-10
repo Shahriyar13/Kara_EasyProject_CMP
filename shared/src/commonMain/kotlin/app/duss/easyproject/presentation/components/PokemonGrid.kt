3ebe266db18e22727585265754f0553627b7a712
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-12-10 15:22:29 +0100

    remeber brush

--- shared/src/commonMain/kotlin/app/duss/easyproject/presentation/components/PokemonGrid.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/presentation/components/PokemonGrid.kt
@@ -16,11 +16,31 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
 import app.duss.easyproject.domain.entity.Project
-import app.duss.easyproject.presentation.ui.project.components.ProjectItem
-import app.duss.easyproject.presentation.ui.project.components.ProjectLoadingItem
+import app.duss.easyproject.presentation.theme.Blue300
+import app.duss.easyproject.presentation.theme.Blue500
+import app.duss.easyproject.presentation.theme.Green300
+import app.duss.easyproject.presentation.theme.Green500
+import app.duss.easyproject.presentation.theme.Indigo300
+import app.duss.easyproject.presentation.theme.Indigo500
+import app.duss.easyproject.presentation.theme.Orange300
+import app.duss.easyproject.presentation.theme.Orange500
+import app.duss.easyproject.presentation.theme.Pink300
+import app.duss.easyproject.presentation.theme.Pink500
+import app.duss.easyproject.presentation.theme.Purple300
+import app.duss.easyproject.presentation.theme.Purple500
+import app.duss.easyproject.presentation.theme.Red300
+import app.duss.easyproject.presentation.theme.Red500
+import app.duss.easyproject.presentation.theme.Teal300
+import app.duss.easyproject.presentation.theme.Teal500
+import app.duss.easyproject.presentation.theme.Yellow300
+import app.duss.easyproject.presentation.theme.Yellow500
+import app.duss.easyproject.presentation.ui.project.list.components.ProjectItem
+import app.duss.easyproject.presentation.ui.project.list.components.ProjectLoadingItem
 
 @Composable
 internal fun PokemonGrid(
@@ -40,6 +60,18 @@ internal fun PokemonGrid(
         )
     )
 
+    val colors = listOf(
+        listOf(Green300, Green500),
+        listOf(Red300, Red500),
+        listOf(Blue300, Blue500),
+        listOf(Purple300, Purple500),
+        listOf(Orange300, Orange500),
+        listOf(Teal300, Teal500),
+        listOf(Pink300, Pink500),
+        listOf(Indigo300, Indigo500),
+        listOf(Yellow300, Yellow500),
+    )
+
     BoxWithConstraints {
         val columns = when(maxWidth) {
             in 0.dp..349.dp -> 1
@@ -49,6 +81,8 @@ internal fun PokemonGrid(
             else -> 5
         }
 
+        var index = 0
+
         LazyVerticalGrid(
             columns = GridCells.Fixed(columns),
             horizontalArrangement = Arrangement.spacedBy(12.dp),
@@ -57,9 +91,14 @@ internal fun PokemonGrid(
             modifier = modifier,
         ) {
             items(projectList, key = { it.code }) { pokemon ->
+                index++
                 ProjectItem(
                     onClick = { onPokemonClicked(pokemon.code) },
                     project = pokemon,
+                    brush = remember(index) {
+                        val colorIndex = index % colors.size
+                        Brush.linearGradient(colors[colorIndex])
+                    },
                     modifier = Modifier.fillMaxWidth()
                 )
             }
