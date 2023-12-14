8a4fa6bf2c3dc7c76132f2bf262c80a4e03b51b8
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-12-14 19:56:04 +0100

    Update ProjectForm.kt

--- shared/src/commonMain/kotlin/app/duss/easyproject/presentation/forms/ProjectForm.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/presentation/forms/ProjectForm.kt
@@ -5,7 +5,7 @@ import app.duss.easyproject.domain.params.toDto
 
 data class ProjectForm(
     override val origin: Project,
-    override val updated: Project = origin.copy(),
+    override var updated: Project = origin.copy(),
 ): BaseForm() {
     override fun toRequestDto() = updated.toDto()
 }
