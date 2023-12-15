9c2d14a9028cb7ec1e430f21c10b58b77b7245cb
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-12-15 04:22:21 +0100

    Update QuotationRequest.kt

--- shared/src/commonMain/kotlin/app/duss/easyproject/domain/params/QuotationRequest.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/domain/params/QuotationRequest.kt
@@ -1,17 +1,23 @@
 package app.duss.easyproject.domain.params
 
+import app.duss.easyproject.domain.entity.Quotation
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class QuotationRequest (
     val id: Long? = null,
-
-    val code: String,
+    val code: String?,
     val time: Long?,
     val confirmTime: Long?,
     val quotationItems: List<QuotationItemRequest> = ArrayList(),
-
     val supplierEnquiryId: Long,
-    val projectId: Long,
+)
 
-    )
+fun Quotation.toDto() = QuotationRequest(
+    id = id,
+    code = code,
+    supplierEnquiryId = supplierEnquiry!!.id!!,
+    time = time,
+    confirmTime = sendTime,
+    quotationItems = quotationItems.map { it.toDto() },
+)
