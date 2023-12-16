db38b5d9d4295feaa1db4364700b067782e56eb8
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-12-16 04:22:51 +0100

    Update EditableText.kt

--- shared/src/commonMain/kotlin/app/duss/easyproject/presentation/components/EditableText.kt
+++ shared/src/commonMain/kotlin/app/duss/easyproject/presentation/components/EditableText.kt
@@ -1,16 +1,15 @@
 package app.duss.easyproject.presentation.components
 
-import androidx.compose.foundation.interaction.InteractionSource
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,17 +18,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun EditableText(
-    modifier: Modifier = Modifier,
     value: String?,
     label: String = "",
     onValueChange: (String?) -> Unit = {},
@@ -40,70 +34,55 @@ internal fun EditableText(
     singleLine: Boolean = true,
     maxLines: Int = if (singleLine) 1 else 4,
     minLines: Int = 1,
-    interactionSource: InteractionSource = MutableInteractionSource(),
+    modifier: Modifier = Modifier
+        .widthIn(max = 440.dp)
+        .padding(horizontal = 20.dp, vertical = 20.dp),
 ) {
     var textValue by remember { mutableStateOf(value) }
     val containerColor = MaterialTheme.colorScheme.surface.copy(alpha = if (readOnly) 0f else .1f)
 
-    BasicTextField(
+    TextField(
         value = textValue ?: "",
         onValueChange = {
             textValue = it
             onValueChange(it)
         },
+        supportingText = {
+            if (!errorMessage.isNullOrEmpty()) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = errorMessage,
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        },
+        trailingIcon = {
+            if (!errorMessage.isNullOrEmpty()) {
+                Icon(
+                    Icons.Filled.Error,
+                    tint = MaterialTheme.colorScheme.error,
+                    contentDescription = errorMessage,
+                )
+            }
+        },
+        label = { Text(label) },
         readOnly = readOnly,
         maxLines = maxLines,
         minLines = minLines,
-        textStyle = TextStyle(
-            fontSize = 16.sp
-        ),
         keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
         singleLine = singleLine,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = containerColor,
+            unfocusedContainerColor = containerColor,
+            disabledContainerColor = containerColor,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedLeadingIconColor = MaterialTheme.colorScheme.surface,
+            unfocusedLeadingIconColor = MaterialTheme.colorScheme.surface,
+            focusedPlaceholderColor = MaterialTheme.colorScheme.surface,
+            unfocusedPlaceholderColor = MaterialTheme.colorScheme.surface,
+        ),
+        shape = MaterialTheme.shapes.extraLarge,
         modifier = modifier,
-    ) {
-        TextFieldDefaults.DecorationBox(
-            value = textValue ?: "",
-            innerTextField = it,
-            singleLine = singleLine,
-            enabled = true,
-            visualTransformation = VisualTransformation.None,
-            label = { Text("$label:") },
-//            label = { if (!readOnly || !textValue.isNullOrEmpty()) Text("$label:") },//TODO make it better
-            trailingIcon = {
-                if (!errorMessage.isNullOrEmpty()) {
-                    Icon(
-                        Icons.Filled.Warning,
-                        tint = MaterialTheme.colorScheme.error,
-                        contentDescription = errorMessage,
-                    )
-                }
-            },
-            supportingText = {
-                if (!errorMessage.isNullOrEmpty()) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = errorMessage,
-                        color = MaterialTheme.colorScheme.error
-                    )
-                }
-            },
-            interactionSource = interactionSource,
-            contentPadding = TextFieldDefaults.contentPaddingWithLabel(
-                start = 8.dp,
-                top = 8.dp
-            ),
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = containerColor,
-                unfocusedContainerColor = containerColor,
-                disabledContainerColor = containerColor,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedLeadingIconColor = MaterialTheme.colorScheme.surface,
-                unfocusedLeadingIconColor = MaterialTheme.colorScheme.surface,
-                focusedPlaceholderColor = MaterialTheme.colorScheme.surface,
-                unfocusedPlaceholderColor = MaterialTheme.colorScheme.surface,
-            ),
-            shape = MaterialTheme.shapes.extraLarge,
-        )
-    }
+    )
 }
